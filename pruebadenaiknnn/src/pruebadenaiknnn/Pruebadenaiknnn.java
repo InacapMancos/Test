2fd229ba5da8ede63d60279598733acc270d564f
KuervoX <dxchxc.dr@gmail.com>
2015-11-04 10:59:04 -0300

    Update Pruebadenaiknnn.java

--- pruebadenaiknnn/src/pruebadenaiknnn/Pruebadenaiknnn.java
+++ pruebadenaiknnn/src/pruebadenaiknnn/Pruebadenaiknnn.java
@@ -9,6 +9,8 @@ public class Pruebadenaiknnn {
      */
     public static void main(String[] args) {
         System.out.println("Manco");
+        System.out.println("hoooo hermano se puede")
+        
     }
     
 }
